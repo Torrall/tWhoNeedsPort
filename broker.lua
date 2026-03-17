86560db6a8763c96c8ba2f37401829113e941608
Torrall <torrall1@gmail.com>
2026-03-17 23:36:12 +0100

    make broker more optional

--- broker.lua
+++ broker.lua
@@ -1,28 +1,33 @@
 local addon, ns = ...
 print("read "..addon..": broker.lua")
 
+if not LibStub then
+    ns.manual_active = true
+    return
+end
+
 local LDB = LibStub("LibDataBroker-1.1", true)
 
-if LDB then
-    ns.broker = LDB:NewDataObject(ns.internal_name, {
-        type  = "data source",
-        label = ns.internal_name,
-        text  = ns.internal_name,
-        icon  = "Interface\\Icons\\Inv12_ability_warlock_ritualofsummoning",
-
-        OnClick = function(_, button)
-            if button == "LeftButton" then
-                ns.manual_active = not(ns.manual_active)
-                ns.check_and_set_active_status()
-            end
-        end,
-
-        OnTooltipShow = function(tooltip)
-            tooltip:AddLine(ns.internal_name)
-            tooltip:AddLine("Left-click to toggle addon (display and checks)", 1, 1, 1)
-        end,
-    })
-else
-    -- if no databroker to toogle - must be active by default
+if not LDB then -- if no databroker to toogle - must be active by default
     ns.manual_active = true
+    return
 end
+
+ns.broker = LDB:NewDataObject(ns.internal_name, {
+    type  = "data source",
+    label = ns.internal_name,
+    text  = ns.internal_name,
+    icon  = "Interface\\Icons\\Inv12_ability_warlock_ritualofsummoning",
+
+    OnClick = function(_, button)
+        if button == "LeftButton" then
+            ns.manual_active = not(ns.manual_active)
+            ns.check_and_set_active_status()
+        end
+    end,
+
+    OnTooltipShow = function(tooltip)
+        tooltip:AddLine(ns.internal_name)
+        tooltip:AddLine("Left-click to toggle addon (display and checks)", 1, 1, 1)
+    end,
+})
